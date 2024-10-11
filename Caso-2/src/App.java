a793f4b6aa12df849b9ec24d9bd226960fcb2aca
Juan Diego Osorio <jd.osorioc1@uniandes.edu.co>
2024-10-10 20:52:00 -0500

    opciones

--- Caso-2/src/App.java
+++ Caso-2/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Escoge una opcion:");
+        System.out.println("1. Option 1");
+        System.out.println("2. Option 2");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println("Opcion 1");
+        } else if (choice == 2) {
+            System.out.println("Opcion 2");
+        } else {
+            System.out.println("Opcion invalida");
+        }
+        scanner.close();
     }
 }
