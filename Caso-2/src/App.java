8f17c3a164e97de5db16e40401a8264b906c1937
Your Name <you@example.com>
2024-10-13 12:34:21 -0500

    Calculo de NR y NP

--- Caso-2/src/App.java
+++ Caso-2/src/App.java
@@ -2,20 +2,35 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Escoge una opcion:");
-        System.out.println("1. Option 1");
-        System.out.println("2. Option 2");
+        System.out.println("Escoge una opción:");
+        System.out.println("1. Generar Referencias");
+        System.out.println("2. Opción 2");
 
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
 
         if (choice == 1) {
-            System.out.println("Opcion 1");
+            System.out.println("Ingrese el tamaño de página (en bytes):");
+            int pageSize = scanner.nextInt();
+            System.out.println("Ingrese el nombre del archivo de la imagen:");
+            String imageFileName = scanner.next();
+            Imagen imagen = new Imagen(imageFileName);
+            generarReferencias(imagen, pageSize);
         } else if (choice == 2) {
-            System.out.println("Opcion 2");
+            System.out.println("Opción 2");
         } else {
-            System.out.println("Opcion invalida");
+            System.out.println("Opción inválida");
         }
         scanner.close();
+    }
+
+    public static void generarReferencias(Imagen imagen, int pageSize) {
+
+        System.out.println("P=" + pageSize);
+        System.out.println("NF=" + imagen.alto); 
+        System.out.println("NC=" + imagen.ancho);
+        System.out.println("NR=" + (imagen.leerLongitud()*17 + 16)); 
+        System.out.println("NP=" + ((imagen.alto * imagen.ancho * 3 / pageSize)));
+
     }
 }
