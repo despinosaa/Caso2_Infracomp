58f85fc187c3dfbd5d3103f73ceafb5057d3807d
Juan Diego Osorio <jd.osorioc1@uniandes.edu.co>
2024-10-14 11:38:19 -0500

    Opcion 1

--- Caso-2/src/App.java
+++ Caso-2/src/App.java
@@ -1,21 +1,160 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Escoge una opcion:");
-        System.out.println("1. Option 1");
-        System.out.println("2. Option 2");
+	int P; // tamanio pagina
+	int NF; // numero filas
+	int NC; // numero columnas
+	int NR; // numero referencias
+	int NP; // numero de paginas virtuales
 
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
+	// Opción 1: generar las referencias
+	public void generarReferencias(Scanner scanner) {
+		System.out.println("Tamanio de pagina: ");
+		P = scanner.nextInt();
+		scanner.nextLine();
 
-        if (choice == 1) {
-            System.out.println("Opcion 1");
-        } else if (choice == 2) {
-            System.out.println("Opcion 2");
-        } else {
-            System.out.println("Opcion invalida");
-        }
-        scanner.close();
-    }
+		System.out.println("ingresa la Ruta (path) de la imagen: ");
+		String input = scanner.nextLine();
+		Imagen imagen = new Imagen(input);
+		int longitud = imagen.leerLongitud();
+
+		System.out.println("P=" + P);
+		char[] vector = new char[longitud];
+		imagen.recuperar(vector, longitud);
+		NF = imagen.alto;
+		NC = imagen.ancho;
+		System.out.println("NF=" + imagen.alto);
+		System.out.println("NC=" + imagen.ancho);
+
+		NR = (longitud * 17) + 16;
+		System.out.println("NR=" + NR);
+
+		double div = (double) longitud / P;
+		int res = (int) Math.ceil(div);
+		NP = ((imagen.alto * 3 * imagen.ancho) / P) + res;
+
+		System.out.println("NP=" + NP);
+		byte[][][] copyImagen = imagen.imagen.clone();
+
+		int longitudMensaje = imagen.leerLongitud();
+
+		char[] colores = { 'R', 'G', 'B' };
+
+		int desplazamiento;
+		int desplazamientoMensaje;
+		int pagMensaje;
+		int posCaracter;
+		int bytesTotales = 0;
+		pagMensaje = NP - res;
+		int bitsCaracter = 0;
+
+		String filePath = "referencias.txt";
+
+		String txtReferencias = "";
+		for (int i = 0; i < copyImagen.length; i++) { // fila i
+			for (int j = 0; j < copyImagen[0].length; j++) { // columna j
+				for (int k = 0; k < copyImagen[0][0].length; k++) { // k = R,G,B
+
+					if (bytesTotales < (longitudMensaje * 8) + 16) {
+
+						if(bytesTotales >= 15 && bytesTotales%8 == 0 && bitsCaracter%8 ==0) {
+							posCaracter = bitsCaracter/8;
+							int nextDespl = (((imagen.alto * 3 * imagen.ancho) + (bitsCaracter/8)))%P;
+							int nextPg = pagMensaje;
+							if(nextDespl == 0 && bytesTotales != 16) {
+								nextPg++;
+							  }
+							  
+							txtReferencias += "Mensaje["+posCaracter+"],"+nextPg+","+nextDespl+",W\n";
+
+						}
+
+						int pagina = bytesTotales / P;
+						desplazamiento = bytesTotales % P;
+						char comp = colores[k % 3];
+						txtReferencias += "Imagen[" + i + "][" + j + "]." + comp + "," + pagina + "," + desplazamiento+ ",R\n";
+
+						if(bytesTotales >= 15) {
+							posCaracter = bitsCaracter/8;
+													  
+							if(bytesTotales != 15) {
+								desplazamientoMensaje = ((imagen.alto * 3 * imagen.ancho) + (bitsCaracter/8))%P;
+							    pagMensaje = ((imagen.alto * 3 * imagen.ancho) + (bitsCaracter/8))/P;
+								txtReferencias += "Mensaje["+posCaracter+"],"+pagMensaje+","+desplazamientoMensaje+",W\n";
+								bitsCaracter++;
+							}
+						}
+
+						bytesTotales++;
+
+					}
+				}
+			}
+		}
+
+		try {
+			FileWriter fileWriter = new FileWriter(filePath);
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			bufferedWriter.write("P=" + P + " y longitud= " + longitud + "\n");
+			bufferedWriter.write("NF=" + NF + "\n");
+			bufferedWriter.write("NC=" + NC + "\n");
+			bufferedWriter.write("NR=" + NR + "\n");
+			bufferedWriter.write("NP=" + NP + "\n");
+			bufferedWriter.write(txtReferencias);
+			bufferedWriter.close();
+
+			System.out.println("Archivo de Referencias generado correctamente.");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+    public static void main(String[] args) {
+		boolean continuar = true;
+		boolean referencias = false;
+		App main = new App();
+
+		while (continuar) {
+			System.out.println("-- Menu de opciones --");
+
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Elija una opción:");
+			System.out.println("1. Generar Referencias.");
+			System.out.println("2. Calcular número de fallas de página, porcentaje de hits, tiempos.");
+			System.out.println("3. Salir");
+
+			int opcion = scanner.nextInt();
+			scanner.nextLine();
+
+			if (opcion == 1) {
+				// Opción 1: Generar referencias de página
+				main.generarReferencias(scanner);
+				referencias = true;
+
+			} else if (opcion == 2) {
+				// Opción 2: Calcular número de fallas de página, hits, tiempos
+				if (!referencias) {
+					System.out.println("Primero debe generar las referencias en la opción 1.");
+				} else {
+					//AQUI VA LA OPCION 2
+                    System.out.println("Calcular datos buscados.");
+				}
+			}
+
+			else if (opcion == 3) {
+				continuar = false;
+				scanner.close();
+			}
+
+			else {
+				System.out.println("Opción no válida. Por favor, elija 1 o 2.");
+			}
+
+		}
+	}
+
 }
